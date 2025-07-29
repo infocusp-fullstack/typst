8389d53fb3ca1da67e4ab57fa0d2da10d5b4bb96
nevil-workspace <nevil@infocusp.com>
2025-07-29 18:23:56 +0530

    update clean script

--- clean.sh
+++ clean.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Install root dependencies
+# Remove deps
 rm -rf node_modules
 rm -f package-lock.json
 rm -f pnpm-lock.yaml
+
+# Optionally Install root dependencies again
+# pnpm install
 
 echo "Cleanup complete!" 
